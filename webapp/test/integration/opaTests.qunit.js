9a0b2f2540b15042a0acddf86ad91d2e17131c85
DD877 <dibyodeo@gmail.com>
2023-01-18 06:49:11 +0000

    version v1

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["democicd/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"democicd/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
